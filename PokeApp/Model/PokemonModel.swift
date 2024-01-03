42e72bf51c4587555f8382e900e47f4d78f0adf7
abdielmg007 <abdiel.mugo@gmail.com>
2024-01-03 02:16:23 -0600

    Structure App

---
+++ PokeApp/Model/PokemonModel.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonModel.swift
+//  PokeApp
+//
+//  Created by Abdiel Mg on 03/01/24.
+//
+
+import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+ 
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID() // posible change for var
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "bulbasaur",
+        url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
---
+++ PokeApp/Services/PokemonServices.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonManagers.swift
+//  PokeApp
+//
+//  Created by Abdiel Mg on 03/01/24.
+//
+
+import Foundation
+
+class PokemonManager{
+    func getPokemon() -> [Pokemon]{
+        let data: PokemonPage = Bundle.main.decode(file: "pokemon.json")
+        let pokemon: [Pokemon] = data.results
+        
+        return pokemon
+    }
+    func getDetailedPokemon(id: Int, _ completion: @escaping(DetailPokemon) -> ()){
+        Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon/\(id)/", model: DetailPokemon.self) { data in
+            completion(data)
+        } failure: { error in
+            print(error)
+        }
+    }
+}
