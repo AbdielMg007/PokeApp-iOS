6bae93758a99171d4080d26a6129bf80de9e6785
abdielmg007 <abdiel.mugo@gmail.com>
2024-01-03 00:36:08 -0600

    Initial Commit

---
+++ PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by Abdiel Mg on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
